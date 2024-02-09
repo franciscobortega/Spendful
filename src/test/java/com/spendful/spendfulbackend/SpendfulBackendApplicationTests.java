08df58875c62ea0e037e06881917e3b9faa81f4e
franciscobortega <fbryanortega@gmail.com>
2024-02-09 08:26:20 -0600

    Merge remote tracking branch into main

---
+++ src/test/java/com/spendful/spendfulbackend/SpendfulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendful.spendfulbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendfulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
