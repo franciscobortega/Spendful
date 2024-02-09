17c850cc6e1b74b4be467eac72f3e29d05919d87
franciscobortega <fbryanortega@gmail.com>
2024-02-09 09:13:15 -0600

    Created expense data model class with attributes

---
+++ src/main/java/com/spendful/spendfulbackend/expense/Expense.java
@@ -0,0 +1,15 @@
+package com.spendful.spendfulbackend.expense;
+
+import java.time.LocalDate;
+
+public class Expense {
+
+    private Long id;
+    private double amount;
+    private LocalDate expenseDate;
+    private String merchant;
+    private String budgetCategory;
+    private String paymentMethod;
+    private String description;
+
+}
