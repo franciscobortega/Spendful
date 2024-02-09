160e91f50dc94821a75884a99e6fd2bac8b60407
franciscobortega <fbryanortega@gmail.com>
2024-02-09 09:16:36 -0600

    Added constructors, getters, and setters to expense model

--- src/main/java/com/spendful/spendfulbackend/expense/Expense.java
+++ src/main/java/com/spendful/spendfulbackend/expense/Expense.java
@@ -12,4 +12,85 @@ public class Expense {
     private String paymentMethod;
     private String description;
 
+    // Constructors
+
+    public Expense() {
+    }
+
+    public Expense(Long id, double amount, LocalDate expenseDate, String merchant, String budgetCategory, String paymentMethod, String description) {
+        this.id = id;
+        this.amount = amount;
+        this.expenseDate = expenseDate;
+        this.merchant = merchant;
+        this.budgetCategory = budgetCategory;
+        this.paymentMethod = paymentMethod;
+        this.description = description;
+    }
+
+    public Expense(double amount, LocalDate expenseDate, String merchant, String budgetCategory, String paymentMethod, String description) {
+        this.amount = amount;
+        this.expenseDate = expenseDate;
+        this.merchant = merchant;
+        this.budgetCategory = budgetCategory;
+        this.paymentMethod = paymentMethod;
+        this.description = description;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getExpenseDate() {
+        return expenseDate;
+    }
+
+    public void setExpenseDate(LocalDate expenseDate) {
+        this.expenseDate = expenseDate;
+    }
+
+    public String getMerchant() {
+        return merchant;
+    }
+
+    public void setMerchant(String merchant) {
+        this.merchant = merchant;
+    }
+
+    public String getBudgetCategory() {
+        return budgetCategory;
+    }
+
+    public void setBudgetCategory(String budgetCategory) {
+        this.budgetCategory = budgetCategory;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
